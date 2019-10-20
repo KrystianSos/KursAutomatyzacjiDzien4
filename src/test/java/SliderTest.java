1046fd83c2cb705a4235b6ebad831f6cdf56af0b
Tester <katarzyna.hejnowicz@gmail.com>
2019-10-20 12:57:22 +0200

    Dodano test przesuwania suwaka w SliderTest

--- src/test/java/SliderTest.java
+++ src/test/java/SliderTest.java
@@ -1,19 +1,45 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class SliderTest extends TestBase {
 
+    public void moveLeft() {
+        driver.findElement(By.id("custom-handle")).sendKeys(Keys.ARROW_LEFT);
+    }
 
-    public void moveTo(int i) {
-        Actions builder = new Actions(driver);
-        driver.findElement(By.id("custom-handle")).click();
+    public void moveRight() {
+        driver.findElement(By.id("custom-handle")).sendKeys(Keys.ARROW_RIGHT);
+    }
+
+
+    public void moveTo(int number) {
+
+
+        int actualValue = 0;
+
+        while (number != actualValue) {
+
+            actualValue = Integer.parseInt(driver.findElement(By.id("custom-handle")).getText());
+
+            if (actualValue < number) {
+                moveRight();
+            } else if (actualValue > number) {
+                moveLeft();
+            }
+        }
+
     }
 
     @Test
     public void TestSlider() {
 
+        driver.get("http://seleniumui.tc-sii.com/slider.php");
+
+        moveTo(50);
+        moveTo(20);
+        moveTo(20);
+        moveTo(75);
 
     }
 
