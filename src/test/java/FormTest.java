36ac6e2ef4141b5fa244592dfa2180cd63a93b23
Tester <katarzyna.hejnowicz@gmail.com>
2019-10-20 11:45:20 +0200

    Dodano losowy wybór roku

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -9,10 +9,20 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.util.List;
+import java.util.Random;
+
 public class FormTest {
 
     WebDriver driver;
     ChromeOptions options;
+
+    public WebElement getRandomElement(List<WebElement> elements) {
+        //Ta metoda przyjmuje listę, a zwraca jeden WebElement
+        Random random = new Random();
+        int randomNumber = random.nextInt(elements.size()-1); //losuje wartość z zadanego przedziału
+        return elements.get(randomNumber); //zwraca element o losowym indeksie. Metoda get jest dostępna z selenium do obsługi list
+    }
 
     @BeforeMethod
     public void setUp(){
@@ -35,14 +45,27 @@ public class FormTest {
         driver.findElement(By.cssSelector("[id='inputEmail3']")).sendKeys("marian@marian.pl");
         driver.findElement(By.cssSelector("[id='gridRadiosMale']")).click();
         driver.findElement(By.cssSelector("[id='inputAge3']")).sendKeys("18");
-        driver.findElement(By.cssSelector("[id='gridRadios3']")).click();
-        driver.findElement(By.cssSelector("[id='gridCheckAutomationTester']")).click();
-        driver.findElement(By.cssSelector("[id='selectContinents']")).click();
-        driver.findElement(By.cssSelector("[value='switch-commands']")).click();
 
+        //To poniżej klika w losowy element z listy
+        List<WebElement> yearsList = driver.findElements(By.name("gridRadiosExperience"));
+        WebElement randomList = getRandomElement(yearsList);
+        randomList.click();
+
+        //driver.findElement(By.cssSelector("[id='gridRadios3']")).click();
+
+        driver.findElement(By.cssSelector("[id='gridCheckAutomationTester']")).click();
+
+
+
+
+        driver.findElement(By.cssSelector("[id='selectContinents']")).click();
         WebElement continentsElement = driver.findElement(By.cssSelector("[id='selectContinents']"));
         Select continentsSelect = new Select(continentsElement);
         continentsSelect.selectByValue("europe");
+
+
+
+        driver.findElement(By.cssSelector("[value='switch-commands']")).click();
 
         driver.findElement(By.cssSelector("[id='chooseFile']")).sendKeys("C:\\Users\\Tester\\pictures\\11.png");
         driver.findElement(By.cssSelector("[id='additionalInformations']")).sendKeys("Dodatkowe informacje");
@@ -51,6 +74,9 @@ public class FormTest {
         Assert.assertEquals(driver.findElement(By.cssSelector("[id='validator-message']")).getText(), "Form send with success");
 
     }
+
+
+
 
     @AfterMethod
     public void tearDown(){
