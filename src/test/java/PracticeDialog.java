dfe07ab31dafb726ef8be8e6f533262578c67309
Krystian Sosnowski <kr.sosnowski@gmail.com>
2019-10-22 07:32:51 +0200

    Test PracticeDialog

--- src/test/java/PracticeDialog.java
+++ src/test/java/PracticeDialog.java
@@ -3,14 +3,14 @@ import org.testng.annotations.Test;
 
 public class PracticeDialog extends TestBase {
 
-    public void fillInForm(){
+    public void fillInForm(String name, String email, String password){
         driver.findElement(By.id("create-user")).click();
         driver.findElement(By.id("name")).clear();
-        driver.findElement(By.id("name")).sendKeys("Marian Mariano");
+        driver.findElement(By.id("name")).sendKeys(name);
         driver.findElement(By.id("email")).clear();
-        driver.findElement(By.id("email")).sendKeys("Marian@Marianowski.pl");
+        driver.findElement(By.id("email")).sendKeys(email);
         driver.findElement(By.id("password")).clear();
-        driver.findElement(By.id("password")).sendKeys("Marian123");
+        driver.findElement(By.id("password")).sendKeys(password);
         driver.findElement(By.xpath(".//button[text()='Create an account']")).click();
 
     }
@@ -18,8 +18,10 @@ public class PracticeDialog extends TestBase {
     @Test
     public void Test(){
         driver.get("http://seleniumui.tc-sii.com/modal-dialog.php");
-        fillInForm();
-
+        fillInForm("Marian Mariano", "mariano@mariano.pl", "marian1");
+        fillInForm("Wojtek woj", "wojtek@mariano.pl", "wojtek1");
+        fillInForm("Wlodek Mariano", "wlodek@mariano.pl", "wlodek23");
+        fillInForm("Agnes Mariano", "agnes@mariano.pl", "agnetysi1");
     }
 
 }
